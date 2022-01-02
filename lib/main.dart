8007b8bd14e4081309a29858c6ca2e9b46b29e35
MisanEtchie <msnetchie11@gmail.com>
2022-01-02 17:22:41 +0100

    fix: button operations

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,13 @@ class CalculatorApp extends StatefulWidget {
 }
 
 class _CalculatorAppState extends State<CalculatorApp> {
+  late double firstNum;
+  late double secondNum;
+  late String history = '';
+  late String textToDisplay = '';
+  late String res;
+  late String operation;
+
   //the calculator button
   Widget CalculatorButton(
     String text,
@@ -25,12 +32,75 @@ class _CalculatorAppState extends State<CalculatorApp> {
         width: 80,
         height: 80,
         child: TextButton(
-          onPressed: () {},
+          onPressed: () {
+            print(text);
+            if (text == "AC") {
+              textToDisplay = '';
+              firstNum = 0;
+              secondNum = 0;
+              res = '';
+              history = '';
+            } else if (text == 'C') {
+              textToDisplay = '';
+              firstNum = 0;
+              secondNum = 0;
+              res = '';
+            } else if (text == '+/-') {
+              if (textToDisplay[0] != '-') {
+                res = '-' + textToDisplay;
+              } else {
+                res = textToDisplay.substring(0);
+              }
+            } else if (text == '←') {
+              res = textToDisplay.substring(0, textToDisplay.length - 1);
+            } else if (text == '+' ||
+                text == '–' ||
+                text == '✕' ||
+                text == '÷ ') {
+              firstNum = double.parse(textToDisplay);
+              res = '';
+              operation = text;
+            } else if (text == '=') {
+              secondNum = double.parse(textToDisplay);
+              if (operation == '+') {
+                res = (firstNum + secondNum).toString();
+                history = firstNum.toString() +
+                    operation.toString() +
+                    secondNum.toString();
+              }
+              if (operation == '–') {
+                res = (firstNum - secondNum).toString();
+                history = firstNum.toString() +
+                    operation.toString() +
+                    secondNum.toString();
+              }
+              if (operation == '✕') {
+                res = (firstNum * secondNum).toString();
+                history = firstNum.toString() +
+                    operation.toString() +
+                    secondNum.toString();
+              }
+              if (operation == '÷') {
+                res = (firstNum / secondNum).toString();
+                history = firstNum.toString() +
+                    operation.toString() +
+                    secondNum.toString();
+              }
+            } else {
+              //res = int.parse(textToDisplay + text).toString();
+              res = double.parse(textToDisplay + text).toString();
+            }
+
+            setState(() {
+              textToDisplay = res;
+            });
+          },
           child: Text(
             text,
             style: TextStyle(
               color: Color(textcolor),
               fontSize: textsize,
+              fontWeight: FontWeight.bold,
             ),
           ),
           style: TextButton.styleFrom(
@@ -62,13 +132,36 @@ class _CalculatorAppState extends State<CalculatorApp> {
               child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
+              Container(
+                alignment: Alignment(1.0, 1.0),
+                child: Padding(
+                  padding: EdgeInsets.only(right: 15),
+                  child: Text(
+                    history,
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment(1.0, 1.0),
+                child: Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Text(
+                    textToDisplay,
+                    style: TextStyle(fontSize: 60),
+                  ),
+                ),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   CalculatorButton("AC", 0xFF9E9E9E, 0xFFFFFFFF, 30),
                   CalculatorButton("C", 0xFF9E9E9E, 0xFFFFFFFF, 30),
                   CalculatorButton("←", 0xFFFFA000, 0xFFFFFFFF, 30),
-                  CalculatorButton("÷", 0xFFFFA000, 0xFFFFFFFF, 30),
+                  CalculatorButton("÷", 0xFFFFA000, 0xFFFFFFFF, 35),
                 ],
               ),
               Row(
@@ -77,7 +170,7 @@ class _CalculatorAppState extends State<CalculatorApp> {
                   CalculatorButton("9", 0xFFEEEEEE, 0xFF000000, 30),
                   CalculatorButton("8", 0xFFEEEEEE, 0xFF000000, 30),
                   CalculatorButton("7", 0xFFEEEEEE, 0xFF000000, 30),
-                  CalculatorButton("X", 0xFFFFA000, 0xFFFFFFFF, 30),
+                  CalculatorButton("✕", 0xFFFFA000, 0xFFFFFFFF, 30),
                 ],
               ),
               Row(
